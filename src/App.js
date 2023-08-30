87c5f83904739abf69f1acf718677d4d231fc2b3
AlexRaven2025 <102300117+AlexRaven2025@users.noreply.github.com>
2023-08-30 12:06:10 -0400

    v0.001

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import { Home }  from './components/home';
+import About from './components/about';
+import Services from './components/services';
+import BookNow from './components/bookNow';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={ <Home />} />
+      <Route path='/about' element={ <About />} />
+      <Route path='/services' element={ <Services />} />
+      <Route path='/booknow' element={ <BookNow />} />
+    </Routes>
+    </>
   );
 }
 
